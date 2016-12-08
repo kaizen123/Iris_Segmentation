818568668f655b2bc8f4d7fdf21aca3c10735bd2
F_fredes <felipe.fredes@ug.uchile.cl>
2016-12-08 15:25:06 -0300

    Función que entrega rgb de imagen j del sujeto i. Se debe especificar lado('L' o 'R') y la constante de re-escalamiento

---
+++ lectura.m
@@ -0,0 +1,14 @@
+function y = lectura(i,j,lado,resize_constant)
+    if i<10
+        carpeta=strcat(0,int2str(i));
+    else
+        carpeta = int2str(i);  
+    end      
+    Nimagen = int2str(j);
+    direccion = strcat('RGB Images\0',carpeta,'\IMG_0',carpeta,'_',lado,'_',Nimagen,'.JPG');
+    %Reescalamiento de imagen
+    I_original = imread(direccion);
+    resize_constant = 1;
+    I = imresize(I_original,resize_constant);
+    y = I;
+end
