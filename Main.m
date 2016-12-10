1bcc31f96e440a01cbfed0d62585e604394d6dbd
F_fredes <felipe.fredes@ug.uchile.cl>
2016-12-10 04:07:18 -0300

    Re subida po error

--- Main.m
+++ Main.m
@@ -1,53 +1,71 @@
+pc_folder = 'Nico';
+carpeta = 'Iris_Segmentation';
+directions = direcciones('Nico', 'Iris_Segmentation');
 
-main_folder = 'RGB Images';
-for i=45:45
-    for j=1:4
-        tic;
-        carpeta = int2str(i);  %% AÑADIR 1 ---> 01, 79 --->79
-        Nimagen = int2str(j);
-        direccion = strcat('RGB Images\0',carpeta,'\IMG_0',carpeta,'_L_',Nimagen,'.JPG');
-        %direccion = strcat('Infrared Images\0',carpeta,'\0',carpeta,'_L','\Img_0',carpeta,'_L_',Nimagen,'.bmp');
-        I_original = imread(direccion);  
+for j=11:11
+    I_original = lectura_2(directions, j, 1);
+
+    % Resizeando (vest verbo) para detección óptima
+    resize_constant = 0.15; 
+    I = imresize(I_original,resize_constant); 
+
+    % Preprocesamiento de imagen
+    gray = rgb2gray(I);
+    equalizada = histeq(gray);
+    binaria = imbinarize(equalizada);
+    J=edge(binaria, 'Canny', 0.05);
+
+    % Radios mínimos y maximos para buscar circulos, encontrados a mano
+    Rmin = floor(60*resize_constant/0.2);
+    Rmax = floor(130*resize_constant/0.2);
+    [centros, radios] = find_circles(J,Rmin,Rmax,0.97);
+
+    % Si se encontraron círculos:
+    if ~isempty(centros);
+        [iris_center, iris_radio] = compare_white_r (binaria, centros, radios);
+
+        %figure      %% BlOQUE PARA EVALUAR DETECCION DE IRIS
+        %imshow(I)   %% I -> Imagen RGB, gray, equailizada, binaria, J           
+        %viscircles(centros, radios, 'EdgeColor','b');
+        %viscircles(iris_center, iris_radio,'EdgeColor','r');
+
+        % Reescalamiento para extraer iris con imagen de la resolución original
+        real_iris_center = iris_center / resize_constant;
+        real_iris_radio = iris_radio / resize_constant;
+        % Mostrar Iris en imagen original
+        %figure, imshow(I_original)
+        %viscircles(real_iris_center, real_iris_radio, 'EdgeColor','b');
+
+        % Mostrar imagen cuadrada solo con el iris circunscrito
+        [iris, iris_square] = just_iris( I_original, real_iris_center, real_iris_radio);
+        %figure, imshow(iris_square),
+
+        % 
+        [centros_pupila, radio_pupila] = finding_retina(iris_square, real_iris_radio);
+
+        % Rellenado de la imagen
+        %no_reflex = sin_reflejos(iris_square, 0.5, 7);
         
-        % Resizeando (vest verbo) para detección óptima
-        resize_constant = 0.15; 
-        I = imresize(I_original,resize_constant); 
+        % Aplicación de filtros medianos
+%         no_reflex_original = no_reflex;       
+%         med = [4 4];
+%         no_reflex(:,:,1) = medfilt2(no_reflex(:,:,1), med);
+%         no_reflex(:,:,2) = medfilt2(no_reflex(:,:,2), med);
+%         no_reflex(:,:,3) = medfilt2(no_reflex(:,:,3), med);
+%         % O gaussianos 
+%         %no_reflex(:,:,1) = imgaussfilt(no_reflex(:,:,1), med);
+%         %no_reflex(:,:,2) = imgaussfilt(no_reflex(:,:,2), med);
+%         %no_reflex(:,:,3) = imgaussfilt(no_reflex(:,:,3), med);
+
+%         figure(),imshowpair(no_reflex_original,no_reflex,'montage')
+
+        % Refinamiento límite límbico
+%         K = 0.5;
+%         resized_original = imresize(I_original, K);
+%         centro = real_iris_center*K;
+%         radio = real_iris_radio*K;
+%         prueba = refinamiento_limite(rgb2gray(I_original), real_iris_center, real_iris_radio);
         
-        % Preprocesamiento de imagen
-        gray = rgb2gray(I);
-        %gray = I;  %% Para infrarojas
-        equalizada = histeq(gray);
-        binaria = imbinarize(equalizada);
-        J=edge(binaria, 'Canny', 0.05);
-        
-        % Radios mínimos y maximos para buscar circulos, encontrados a mano
-        Rmin = floor(60*resize_constant/0.2);
-        Rmax = floor(130*resize_constant/0.2);
-        [centros, radios] = find_circles(J,Rmin,Rmax,0.97);
-        
-        % Si se encontraron círculos:
-        if ~isempty(centros);
-            [iris_center, iris_radio] = compare_white_r (binaria, centros, radios);
-            
-            %figure      %% BlOQUE PARA EVALUAR DETECCION DE IRIS
-            %imshow(I)   %% I -> Imagen RGB, gray, equailizada, binaria, J           
-            %viscircles(centros, radios, 'EdgeColor','b');
-            %viscircles(iris_center, iris_radio,'EdgeColor','r');
-            
-            % Reescalamiento para extraer iris con imagen de la resolución
-            % original
-            real_iris_center = iris_center / resize_constant;
-            real_iris_radio = iris_radio / resize_constant;
-            [iris, iris_square] = just_iris( I_original, real_iris_center, real_iris_radio);
-            figure
-            A = umbral(rgb2gray(iris_square),25 ,1);
-            imshow(A); 
-            figure, imshow(iris_square),
-            
-            %centros_pupila = finding_retina(iris_square, real_iris_radio);
-        end
-        toc;
-  
     end
 end
 
